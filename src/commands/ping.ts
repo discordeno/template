ec4dde9916e691f17f6b686facd10f9a4f59c04b
Skillz <gankstarsleague@gmail.com>
2020-03-19 16:35:16 -0400

    finalize ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,6 @@
-console.log("ping command was read")
 import { Message } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/structures/message.ts"
 import { bot_cache } from "../../mod.ts"
-import {
-  logGreen,
-  logBlue,
-  logRed
-} from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/utils/logger.ts"
-import { cache } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/utils/cache.ts"
+
 
 export const ping_command = (message: Message) => {
   return message.channel().send_message("testing ping")
