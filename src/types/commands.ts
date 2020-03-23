763d5c4fa5c2931155dd28fb8d176dff969d1a35
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2020-03-23 09:07:19 -0400

    Update commands.ts

--- src/types/commands.ts
+++ src/types/commands.ts
@@ -1,6 +1,7 @@
 import { Message } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/structures/message.ts";
 
 export interface Command {
+	guild_only?: boolean
 	nsfw?: boolean
 	callback: (message: Message, args: string[]) => unknown
 }
