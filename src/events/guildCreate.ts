ec881e8fb9aeec15685941da0c67993df4b65400
Skillz <gankstarsleague@gmail.com>
2020-05-15 09:37:36 -0400

    cleaner guildCreate log

--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,5 +1,8 @@
 import { Guild } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/v1/structures/guild.ts";
+import { logGreen } from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/v1/utils/logger.ts";
 
 export const guildCreate = (guild: Guild) => {
-  console.log(guild.name);
+  logGreen(
+    `[EVENT=GuildCreate]: ${guild.name} with ${guild.memberCount} members.`,
+  );
 };
