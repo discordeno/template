b37c707a5afd4cc6364aa915160a640837fcdf93
Skillz <gankstarsleague@gmail.com>
2020-08-07 23:19:52 -0400

    add deps.ts file

---
+++ deps.ts
@@ -0,0 +1,5 @@
+export * from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/v7/mod.ts"
+
+import Client from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/v7/src/module/client.ts";
+
+export default Client
