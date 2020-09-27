41cce17d72607b4964c4e5e09fd47a21bb229aae
Skillz <gankstarsleague@gmail.com>
2020-09-26 21:07:35 -0400

    use nestland again

--- deps.ts
+++ deps.ts
@@ -1,10 +1,6 @@
-// export * from "https://x.nest.land/Discordeno@8.7.2/mod.ts";
-export * from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/mod.ts";
-
-// import Client from "https://x.nest.land/Discordeno@8.7.2/src/module/client.ts";
-import Client from "https://raw.githubusercontent.com/Skillz4Killz/Discordeno/master/src/module/client.ts";
-
 export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.67.0/async/delay.ts";
+export * from "https://x.nest.land/Discordeno@9.0.1/mod.ts";
 
+import Client from "https://x.nest.land/Discordeno@9.0.1/src/module/client.ts";
 export default Client;
