b6ece3102d0003b5a168bebe8f7a08a506c44efe
ayntee <ayyantee@gmail.com>
2020-11-06 20:31:07 +0400

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -2,8 +2,4 @@ export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.67.0/async/delay.ts";
 export * from "https://deno.land/x/discordeno@9.0.15/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.0/mod.ts";
-export * from "./cache.ts";
-
-import Client from "https://deno.land/x/discordeno@9.0.15/src/module/client.ts";
-
-export default Client;
+export { default as Client } from "https://deno.land/x/discordeno@9.0.15/src/module/client.ts";
