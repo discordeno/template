664e9f88ba378ad7a2af3df7a96127bd43a4e5c6
ayntee <ayyantee@gmail.com>
2020-11-24 09:50:42 +0400

    Inlines replies

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.67.0/async/delay.ts";
-export * from "https://deno.land/x/discordeno@9.0.16/mod.ts";
+export * from "https://deno.land/x/discordeno@9.4.0/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.0/mod.ts";
 export * from "./cache.ts";
 
-import Client from "https://deno.land/x/discordeno@9.0.16/src/module/client.ts";
+import Client from "https://deno.land/x/discordeno@9.4.0/src/module/client.ts";
 
 export default Client;
