bac815842920d3cbbecfaf894d717637c6c153a8
Ayyan <ayyantee@gmail.com>
2020-12-18 18:48:51 +0400

    deps: update discordeno to 9.4.1

--- deps.ts
+++ deps.ts
@@ -1,9 +1,5 @@
 export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
 export * from "https://deno.land/std@0.67.0/async/delay.ts";
-export * from "https://deno.land/x/discordeno@9.4.0/mod.ts";
+export * from "https://deno.land/x/discordeno@9.4.1/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
 export * from "./cache.ts";
-
-import Client from "https://deno.land/x/discordeno@9.4.0/src/module/client.ts";
-
-export default Client;
