793534c873699ac88acdd14b789c62f93c0ca03a
Ayyan <ayyantee@gmail.com>
2020-12-26 19:07:53 +0400

    chore: remove default export

--- deps.ts
+++ deps.ts
@@ -3,7 +3,3 @@ export * from "https://deno.land/std@0.67.0/async/delay.ts";
 export * from "https://deno.land/x/discordeno@9.4.0/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
 export * from "./cache.ts";
-
-import Client from "https://deno.land/x/discordeno@9.4.0/src/module/client.ts";
-
-export default Client;
