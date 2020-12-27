506c4da64bd1c5612ba9a399dcd34eb596e6e828
Skillz <gankstarsleague@gmail.com>
2020-12-27 13:45:37 -0500

    use deno.land

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
-export * from "https://raw.githubusercontent.com/discordeno/discordeno/master/mod.ts";
+export * from "https://deno.land/x/discordeno@10.0.0/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
 export * from "./cache.ts";
