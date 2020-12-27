19cbf4b91b29b9c658b01f4665531dfb1fb543d5
Skillz <gankstarsleague@gmail.com>
2020-12-27 10:32:10 -0500

    prepare for v10

--- deps.ts
+++ deps.ts
@@ -1,5 +1,4 @@
-export * from "https://deno.land/std@0.67.0/fmt/colors.ts";
-export * from "https://deno.land/std@0.67.0/async/delay.ts";
-export * from "https://deno.land/x/discordeno@9.4.0/mod.ts";
+export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
+export * from "https://deno.land/x/discordeno@10.0.0/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
 export * from "./cache.ts";
