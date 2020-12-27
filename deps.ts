220d99de113d8f045c7fc04f4be09368acdefc7f
Skillz <gankstarsleague@gmail.com>
2020-12-27 14:40:07 -0500

    fix imports

--- deps.ts
+++ deps.ts
@@ -2,7 +2,3 @@ export * from "https://deno.land/std@0.81.0/fmt/colors.ts";
 export * from "https://deno.land/x/discordeno@10.0.0/mod.ts";
 export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
 export * from "./cache.ts";
-
-import Client from "https://deno.land/x/discordeno@9.4.0/src/module/client.ts";
-
-export default Client;
