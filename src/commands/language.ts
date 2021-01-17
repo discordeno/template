5b7acedaf0d88b54f6df676c48ba141a274ce621
Myssiry <71568768+Myssiry@users.noreply.github.com>
2021-01-17 19:30:03 +0100

    language cmd - list languages, better usage & ui

--- src/commands/language.ts
+++ src/commands/language.ts
@@ -3,7 +3,8 @@ import { PermissionLevels } from "../types/commands.ts";
 import { createSubcommand, createCommand, getCurrentLanguage } from "../utils/helpers.ts";
 import { Embed } from "../utils/Embed.ts";
 import { db } from "../database/database.ts";
-import { configs } from "../../configs.ts";
+
+const allowedLanguages = [ { id: "en_US", flag: ":flag_us:", name: "English"}, { id: "cs_CZ", flag:":flag_cz:", name: "Czech"}];
 
 createCommand({
   name: "language",
@@ -18,9 +19,11 @@ createCommand({
   guildOnly: true,
   permissionLevels: [PermissionLevels.MEMBER],
   execute: async function (message) {
+    const currentLanguageId = getCurrentLanguage(message.guildID) || 'en_US';
+    let currentLanguage = allowedLanguages.find(item => item.id === currentLanguageId) || allowedLanguages[0];
     const embed = new Embed()
       .setTitle("Language Information")
-      .setDescription(`**Current Language**: \`${getCurrentLanguage(message.guildID)}\``)
+      .setDescription(`**Current Language**: ${currentLanguage.flag} - \`${currentLanguage.name}\``)
       .setTimestamp();
 
     await message.send({ embed }).catch(console.log);
@@ -35,21 +38,50 @@ createSubcommand("language", {
       type: "string",
       required: true,
       missing: async function(message) {
-        message.reply(`Please provide a language`);
+        const listOfLanguages = allowedLanguages.map(lang => `${lang.flag} - \`${lang.name}\``).join('\n');
+        const embed = new Embed()
+          .setTitle("Available Languages")
+          .setDescription(listOfLanguages);
+        await message.send({ embed}).catch(console.log);
       },
     },
   ],
   permissionLevels: [PermissionLevels.ADMIN],
   execute: async function (message, args) {
-    const oldLanguage = getCurrentLanguage(message.guildID);
-    botCache.guildLanguages.set(message.guildID, args.language);
-    await db.guilds.update(message.guildID, { language: args.language });
+    //Old
+    const oldLanguageId = getCurrentLanguage(message.guildID) || 'en_US';
+    const oldLanguage = allowedLanguages.find(item => item.id === oldLanguageId) || allowedLanguages[0];
 
-    const embed = new Embed()
-      .setTitle("Success, language was changed")
-      .setDescription([`**Old Language**: \`${oldLanguage}\``, `**New Language**: \`${args.language}\``])
-      .setTimestamp();
+    //New
+    const newLanguageName = args.language;
+    const newLanguage = allowedLanguages.find(item => item.name === newLanguageName);
 
-    await message.send({ embed }).catch(console.log);
+    //Handle
+    if (!newLanguage) {
+      const embed = new Embed()
+        .setTitle("Error")
+        .setDescription('Check list of languages by running command `language set`.')
+        .addField('Error', `\`${args.language}\` is not a valid language`);
+
+        await message.send({ embed }).catch(console.log);
+    } else if(newLanguage.id === oldLanguageId) {
+      const embed = new Embed()
+        .setTitle("Error")
+        .setDescription('Check list of languages by running command `language set`.')
+        .addField('Error', `\`${args.language}\` is already set.`);
+
+        await message.send({ embed }).catch(console.log);
+    }
+    else {
+      botCache.guildLanguages.set(message.guildID, newLanguage.id);
+      await db.guilds.update(message.guildID, { language: newLanguage.id });
+
+      const embed = new Embed()
+        .setTitle("Success")
+        .setDescription([`**Old Language**: ${oldLanguage.flag} - \`${oldLanguage.name}\``, `**New Language**: ${newLanguage.flag} - \`${newLanguage.name}\``])
+        .setTimestamp();
+
+      await message.send({ embed }).catch(console.log);
+    }
   },
 });
