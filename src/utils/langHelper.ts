9bc6e274196135dea42fd228a5df33131fa09c40
Myssiry <71568768+Myssiry@users.noreply.github.com>
2021-01-17 16:54:12 +0100

    Update src/utils/langHelper.ts
    Co-authored-by: Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>

--- src/utils/langHelper.ts
+++ src/utils/langHelper.ts
@@ -2,7 +2,7 @@ import { botCache } from "../../deps.ts";
 import { db } from "../database/database.ts";
 
 const loadLanguages = async () => {
-    let guilds = await db.guilds.getAll();
+    let guilds = await db.guilds.getAll(true).catch(console.log);
     for(const guild of guilds) {
         if (guild[1].language) {
             botCache.guildLanguages.set(guild[1].id, guild[1].language);
