4d7a476f3a6173d000b054552fab8a0ac4075331
Myssiry <71568768+Myssiry@users.noreply.github.com>
2021-01-18 10:02:58 +0100

    lang cmd - interface renamed

--- src/commands/language.ts
+++ src/commands/language.ts
@@ -50,7 +50,7 @@ createSubcommand("language", {
     },
   ],
   permissionLevels: [PermissionLevels.ADMIN],
-  execute: async function (message, args: SetArgs) {
+  execute: async function (message, args: LanguageSetArgs) {
     //Old
     const oldLanguageId = getCurrentLanguage(message.guildID);
     const oldLanguage = allowedLanguages.find(item => item.id === oldLanguageId) || allowedLanguages[0];
@@ -81,7 +81,7 @@ createSubcommand("language", {
   },
 });
 
-interface SetArgs {
+interface LanguageSetArgs {
   language?:
     | "Czech"
     | "English"
