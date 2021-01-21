6a16fb6af180a5193149f962bc08f4cff02aece7
ITOH <72305210+itohatweb@users.noreply.github.com>
2021-01-21 14:54:39 +0100

    add(utils): better import function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -122,22 +122,54 @@ export function editEmbed(message: Message, embed: Embed, content?: string) {
 
 // Very important to make sure files are reloaded properly
 let uniqueFilePathCounter = 0;
+let paths: string[] = [];
+
 /** This function allows reading all files in a folder. Useful for loading/reloading commands, monitors etc */
 export async function importDirectory(path: string) {
   const files = Deno.readDirSync(Deno.realPathSync(path));
+  const folder = path.substring(path.indexOf("/src/") + 5);
+  if (!folder.includes("/")) console.log(`Loading ${folder}...`);
 
   for (const file of files) {
     if (!file.name) continue;
 
-    const currentPath = `${path}/${file.name}`;
+    const currentPath = `${path}/${file.name}`.replaceAll("\\", "/");
     if (file.isFile) {
-      await import(`file:///${currentPath}#${uniqueFilePathCounter}`);
+      if (!currentPath.endsWith(".ts")) continue;
+      paths.push(
+        `import "${Deno.mainModule.substring(
+          0,
+          Deno.mainModule.lastIndexOf("/")
+        )}/${currentPath.substring(
+          currentPath.indexOf("src/")
+        )}#${uniqueFilePathCounter}";`
+      );
       continue;
     }
 
     await importDirectory(currentPath);
   }
+
   uniqueFilePathCounter++;
+}
+
+/** Imports all everything in fileloader.ts */
+export async function fileLoader() {
+  await Deno.writeTextFile(
+    "fileloader.ts",
+    paths.join("\n").replaceAll("\\", "/")
+  );
+  await import(
+    `${Deno.mainModule.substring(
+      0,
+      Deno.mainModule.lastIndexOf("/")
+    )}/fileloader.ts#${uniqueFilePathCounter}`
+  );
+  resetPaths();
+}
+
+export function resetPaths() {
+  paths = [];
 }
 
 export function getTime() {
