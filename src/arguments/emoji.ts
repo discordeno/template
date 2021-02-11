291b3264b25d05a26d5851e25b0115b4dfc0234d
TriForMine <quentin.nicolini@hotmail.fr>
2021-02-11 17:18:24 +0100

    Add emoji arguments and add createEmbedsPagination function

---
+++ src/arguments/emoji.ts
@@ -0,0 +1,14 @@
+import { botCache } from "../../cache.ts";
+import {EMOJI_REGEX} from "../utils/constants/emoj_regex.ts";
+
+botCache.arguments.set("emoji", {
+    name: "emoji",
+    execute: function (argument, parameters) {
+        const [text] = parameters;
+
+        const match = text?.match(EMOJI_REGEX);
+
+        if (match && match.length > 0)
+            return match.join('');
+    },
+});
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,16 +1,19 @@
 import { Command } from "../types/commands.ts";
 import { Embed } from "./Embed.ts";
 import {
+  addReactions,
   botCache,
+  cache,
   Collection,
-  MessageContent,
-  Message,
-  sendMessage,
   deleteMessageByID,
   editMessage,
-  cache,
+  Message,
+  MessageContent,
+  removeUserReaction,
+  sendMessage,
 } from "../../deps.ts";
 import { Milliseconds } from "./constants/time.ts";
+import { needMessage, needReaction } from "./collectors.ts";
 
 /** This function should be used when you want to convert milliseconds to a human readable format like 1d5h. */
 export function humanizeMilliseconds(milliseconds: number) {
@@ -78,14 +81,20 @@ export function createCommand(command: Command) {
   botCache.commands.set(command.name, command);
 }
 
-export function createSubcommand(commandName: string, subcommand: Command, retries = 0) {
+export function createSubcommand(
+  commandName: string,
+  subcommand: Command,
+  retries = 0,
+) {
   const names = commandName.split("-");
 
   let command = botCache.commands.get(commandName);
 
   if (names.length > 1) {
     for (const name of names) {
-      const validCommand = command ? command.subcommands?.get(name) : botCache.commands.get(name);
+      const validCommand = command
+        ? command.subcommands?.get(name)
+        : botCache.commands.get(name);
       if (!validCommand) break;
 
       command = validCommand;
@@ -95,11 +104,16 @@ export function createSubcommand(commandName: string, subcommand: Command, retri
   if (!command) {
     // If 10 minutes have passed something must have been wrong
     if (retries === 20) {
-      return console.error(`Subcommand ${subcommand} unable to be created for ${commandName}`);
+      return console.error(
+        `Subcommand ${subcommand} unable to be created for ${commandName}`,
+      );
     }
 
     // Try again in 30 seconds in case this command file just has not been loaded yet.
-    setTimeout(() => createSubcommand(commandName, subcommand, retries++), 30000);
+    setTimeout(
+      () => createSubcommand(commandName, subcommand, retries++),
+      30000,
+    );
     return;
   }
 
@@ -137,12 +151,16 @@ export async function importDirectory(path: string) {
     if (file.isFile) {
       if (!currentPath.endsWith(".ts")) continue;
       paths.push(
-        `import "${Deno.mainModule.substring(
-          0,
-          Deno.mainModule.lastIndexOf("/")
-        )}/${currentPath.substring(
-          currentPath.indexOf("src/")
-        )}#${uniqueFilePathCounter}";`
+        `import "${
+          Deno.mainModule.substring(
+            0,
+            Deno.mainModule.lastIndexOf("/"),
+          )
+        }/${
+          currentPath.substring(
+            currentPath.indexOf("src/"),
+          )
+        }#${uniqueFilePathCounter}";`,
       );
       continue;
     }
@@ -157,17 +175,18 @@ export async function importDirectory(path: string) {
 export async function fileLoader() {
   await Deno.writeTextFile(
     "fileloader.ts",
-    paths.join("\n").replaceAll("\\", "/")
+    paths.join("\n").replaceAll("\\", "/"),
   );
   await import(
-    `${Deno.mainModule.substring(
-      0,
-      Deno.mainModule.lastIndexOf("/")
-    )}/fileloader.ts#${uniqueFilePathCounter}`
+    `${
+      Deno.mainModule.substring(
+        0,
+        Deno.mainModule.lastIndexOf("/"),
+      )
+    }/fileloader.ts#${uniqueFilePathCounter}`
   );
   paths = [];
 }
-
 
 export function getTime() {
   const now = new Date();
@@ -181,9 +200,130 @@ export function getTime() {
     hour = hour - 12;
   }
 
-  return `${hour >= 10 ? hour : `0${hour}`}:${minute >= 10 ? minute : `0${minute}`} ${amOrPm}`;
+  return `${hour >= 10 ? hour : `0${hour}`}:${
+    minute >= 10 ? minute : `0${minute}`
+  } ${amOrPm}`;
 }
 
 export function getCurrentLanguage(guildID: string) {
-  return botCache.guildLanguages.get(guildID) || cache.guilds.get(guildID)?.preferredLocale || 'en_US';
+  return botCache.guildLanguages.get(guildID) ||
+    cache.guilds.get(guildID)?.preferredLocale || "en_US";
+}
+
+/** This function allows to create a pagination using embeds and reactions Requires GUILD_MESSAGE_REACTIONS intent **/
+export async function createEmbedsPagination(
+  channelID: string,
+  authorID: string,
+  embeds: Embed[],
+  defaultPage = 1,
+  reactionTimeout = Milliseconds.SECOND * 30,
+  reactions: {
+    [emoji: string]: (
+      setPage: (newPage: number) => void,
+      currentPage: number,
+      pageCount: number,
+      deletePagination: () => void,
+    ) => Promise<void>;
+  } = {
+    "◀️": async (setPage, currentPage) => setPage(Math.max(currentPage - 1, 1)),
+    "↗️": async (setPage) => {
+      const question = await sendMessage(
+        channelID,
+        "To what page would you like to jump? Say `cancel` or `0` to cancel the prompt.",
+      );
+      const answer = await needMessage(authorID, channelID);
+
+      if (question) {
+        await deleteMessageByID(question.channelID, question.id);
+      }
+
+      const newPageNumber = parseInt(answer.content);
+
+      if (answer) {
+        await deleteMessageByID(answer.channelID, answer.id);
+      }
+
+      if (isNaN(newPageNumber)) {
+        await sendMessage(channelID, "This is not a valid number!");
+        return;
+      }
+
+      if (newPageNumber < 1 || newPageNumber > embeds.length) {
+        await sendMessage(channelID, `This is not a valid page!`);
+        return;
+      }
+
+      setPage(newPageNumber);
+    },
+    "▶️": async (setPage, currentPage, pageCount) =>
+      setPage(Math.min(currentPage + 1, pageCount)),
+    "🗑️": async (setPage, currentPage, pageCount, deletePagination) =>
+      deletePagination(),
+  },
+) {
+  if (embeds.length === 0) {
+    return;
+  }
+
+  let currentPage = defaultPage;
+  const embedMessage = await sendEmbed(channelID, embeds[currentPage - 1]);
+
+  if (!embedMessage) {
+    return;
+  }
+
+  if (embeds.length <= 1) {
+    return;
+  }
+
+  await addReactions(
+    embedMessage.channelID,
+    embedMessage.id,
+    Object.keys(reactions),
+    true,
+  );
+
+  let isEnded = false;
+
+  while (!isEnded) {
+    if (!embedMessage) {
+      return;
+    }
+    const reaction = await needReaction(authorID, embedMessage.id, {
+      duration: reactionTimeout,
+    });
+    if (!reaction) {
+      return;
+    }
+
+    if (embedMessage.guildID) {
+      await removeUserReaction(
+        embedMessage.channelID,
+        embedMessage.id,
+        reaction,
+        authorID,
+      );
+    }
+
+    if (reactions[reaction]) {
+      await reactions[reaction](
+        (newPage) => {
+          currentPage = newPage;
+        },
+        currentPage,
+        embeds.length,
+        () => {
+          isEnded = true;
+          deleteMessageByID(embedMessage.channelID, embedMessage.id);
+        },
+      );
+    }
+
+    if (
+      isEnded || !embedMessage ||
+      !(await editEmbed(embedMessage, embeds[currentPage - 1]))
+    ) {
+      return;
+    }
+  }
 }
