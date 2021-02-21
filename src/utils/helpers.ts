bcc08a07314985c00c97f2e78e9f8e1a822722af
Pawiiiiii <pwii+gh@protonmail.com>
2021-02-21 15:10:32 +0100

    add support for seconds in stringToMilliseconds

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -36,7 +36,7 @@ export function humanizeMilliseconds(milliseconds: number) {
 
 /** This function helps convert a string like 1d5h to milliseconds. */
 export function stringToMilliseconds(text: string) {
-  const matches = text.match(/(\d+[w|d|h|m]{1})/g);
+  const matches = text.match(/(\d+[w|d|h|m|s]{1})/g);
   if (!matches) return;
 
   let total = 0;
@@ -65,6 +65,9 @@ export function stringToMilliseconds(text: string) {
         break;
       case `m`:
         multiplier = Milliseconds.MINUTE;
+        break;
+      case `s`:
+        multiplier = Milliseconds.SECOND;
         break;
     }
 
