df19bacbdd803ff206b307592240f951979b92f7
ayntee <ayyantee@gmail.com>
2021-03-11 17:46:46 +0400

    deps: update to latest (#106)

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/std@0.88.0/fmt/colors.ts";
-export * from "https://deno.land/x/discordeno@10.4.0/mod.ts";
-export * from "https://deno.land/x/sabr@1.1.2/mod.ts";
+export * from "https://deno.land/std@0.90.0/fmt/colors.ts";
+export * from "https://deno.land/x/discordeno@10.5.0/mod.ts";
+export * from "https://deno.land/x/sabr@1.1.4/mod.ts";
 export * from "./cache.ts";
