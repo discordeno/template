80ec80ad0a6352d119cbdd4f2ffe49e45ce0eb71
TriForMine <quentin.nicolini@hotmail.fr>
2021-05-02 00:20:46 +0200

    Add test command

---
+++ src/commands/test.ts
@@ -0,0 +1,21 @@
+import { Embed } from "./../utils/Embed.ts";
+import {createCommand, createEmbedsButtonsPagination, createEmbedsPagination} from "../utils/helpers.ts";
+
+createCommand({
+  name: `test`,
+  guildOnly: true,
+  execute: async (message) => {
+
+    const embeds: Embed[] = [];
+
+    for (let i = 0; i < 10; i++)
+      embeds.push(new Embed()
+          .setTitle(`Page ${i}`)
+          .setTimestamp(Date.now())
+          .setAuthor(`${message.guild?.botMember?.nick || message.guild?.bot?.tag} Stats`, message.guild?.bot?.avatarURL)
+          .setColor("random")
+      )
+
+    return createEmbedsButtonsPagination(message.id, message.channelId, message.author.id, embeds);
+  },
+});
