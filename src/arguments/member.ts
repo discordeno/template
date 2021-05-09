87383dc1df30664c7b2c36823ddaf79631fee336
ITOH <to@itoh.at>
2021-05-09 21:10:43 +0200

    fix member arg

--- src/arguments/member.ts
+++ src/arguments/member.ts
@@ -1,5 +1,6 @@
-import { cache, fetchMembers, snowflakeToBigint } from "../../deps.ts";
+import { cache, snowflakeToBigint } from "../../deps.ts";
 import { bot } from "../../cache.ts";
+import { fetchMember } from "../utils/helpers.ts";
 
 bot.arguments.set("member", {
   name: "member",
@@ -10,17 +11,13 @@ bot.arguments.set("member", {
     const guild = cache.guilds.get(message.guildId);
     if (!guild) return;
 
-    const userId = id.startsWith("<@")
-      ? id.substring(id.startsWith("<@!") ? 3 : 2, id.length - 1)
-      : id;
+    const userId = id.startsWith("<@") ? id.substring(id.startsWith("<@!") ? 3 : 2, id.length - 1) : id;
 
     const cachedMember = cache.members.get(snowflakeToBigint(userId));
     if (cachedMember?.guilds.has(message.guildId)) return cachedMember;
 
     const cached = cache.members.find(
-      (member) =>
-        member.guilds.has(message.guildId) &&
-        member.tag.toLowerCase().startsWith(userId.toLowerCase()),
+      (member) => member.guilds.has(message.guildId) && member.tag.toLowerCase().startsWith(userId.toLowerCase())
     );
     if (cached) return cached;
 
@@ -30,12 +27,8 @@ bot.arguments.set("member", {
 
     console.log("Fetching a member with Id from gateway", userId);
 
-    const member = await fetchMembers(guild.id, guild.shardId, {
-      userIds: [userId],
-      limit: 1,
-    }).catch(console.log);
-    if (!member) return;
+    const member = await fetchMember(guild.id, userId);
 
-    return member.first();
+    return member;
   },
 });
