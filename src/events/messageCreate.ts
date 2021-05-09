3d2efb06319233b7028f2123517e5ec59fc1d9f4
ITOH <to@itoh.at>
2021-05-09 20:47:53 +0200

    make perms check more efficient

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -7,6 +7,7 @@ import {
   hasGuildPermissions,
 } from "../../deps.ts";
 import { bot } from "../../cache.ts";
+import { fetchMember } from "../utils/helpers.ts";
 
 // deno-lint-ignore require-await
 bot.eventHandlers.messageCreate = async function (message) {
@@ -16,10 +17,7 @@ bot.eventHandlers.messageCreate = async function (message) {
     // The !== false is important because when not provided we default to true
     if (monitor.ignoreBots !== false && message.isBot) return;
 
-    if (
-      monitor.ignoreDM !== false &&
-      message.channel?.type === DiscordChannelTypes.DM
-    ) {
+    if (monitor.ignoreDM !== false && message.channel?.type === DiscordChannelTypes.DM) {
       return;
     }
 
@@ -39,51 +37,30 @@ bot.eventHandlers.messageCreate = async function (message) {
     }
 
     // If some permissions is required it must be in a guild
-    if (!message.guild) return;
+    if (!message.guildId) return;
 
-    // Check if the message author has the necessary channel permissions to run this monitor
-    if (monitor.userChannelPermissions) {
-      const results = await Promise.all(
-        monitor.userChannelPermissions.map((perm) =>
-          hasChannelPermissions(message.channelId, message.authorId, [perm])
-        ),
-      );
-      if (results.includes(false)) return;
-    }
+    // Fetch the member if not in cache in rare rare edge cases it can be undefined
+    const member = await fetchMember(message.guildId, message.authorId);
+    if (!member) return;
 
-    // Check if the message author has the necessary permissions to run this monitor
-    if (
-      monitor.userServerPermissions &&
-      !(await hasGuildPermissions(
-        message.guildId,
-        message.authorId,
-        monitor.userServerPermissions,
-      ))
-    ) {
-      return;
-    }
+    const permissionCheckResults = await Promise.all([
+      // Check if the message author has the necessary channel permissions to run this monitor
+      monitor.userChannelPermissions
+        ? hasChannelPermissions(message.channelId, member, monitor.userChannelPermissions)
+        : undefined,
+      // Check if the message author has the necessary guild permissions to run this monitor
+      monitor.userServerPermissions
+        ? hasGuildPermissions(message.guildId, member, monitor.userServerPermissions)
+        : undefined,
+      // Check if the bot has the necessary channel permissions to run this monitor in this channel.
+      monitor.botChannelPermissions
+        ? botHasChannelPermissions(message.channelId, monitor.botChannelPermissions)
+        : undefined,
+      // Check if the bot has the necessary guild permissions to run this monitor
+      monitor.botServerPermissions ? botHasGuildPermissions(message.guildId, monitor.botServerPermissions) : undefined,
+    ]);
 
-    // Check if the bot has the necessary channel permissions to run this monitor in this channel.
-    if (
-      monitor.botChannelPermissions &&
-      !(await botHasChannelPermissions(
-        message.channelId,
-        monitor.botChannelPermissions,
-      ))
-    ) {
-      return;
-    }
-
-    // Check if the bot has the necessary permissions to run this monitor
-    if (
-      monitor.botServerPermissions &&
-      !(await botHasGuildPermissions(
-        message.guildId,
-        monitor.botServerPermissions,
-      ))
-    ) {
-      return;
-    }
+    if (permissionCheckResults.includes(false)) return;
 
     return monitor.execute(message);
   });
