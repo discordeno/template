d2bc98317a275c6f034cd0d2d4792dad11293340
itohatweb <itohatweb@users.noreply.github.com>
2021-05-10 13:23:31 +0000

    Commit from GitHub Actions (Lint)

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,4 +1,10 @@
-import { botId, cache, DiscordActivityTypes, editBotStatus, upsertSlashCommands } from "../../deps.ts";
+import {
+  botId,
+  cache,
+  DiscordActivityTypes,
+  editBotStatus,
+  upsertSlashCommands,
+} from "../../deps.ts";
 import { Command } from "../types/commands.ts";
 import { Milliseconds } from "../utils/constants/time.ts";
 import { getTime } from "../utils/helpers.ts";
@@ -21,7 +27,10 @@ bot.eventHandlers.ready = async function () {
 
   console.log(getTime(), `Loaded ${bot.arguments.size} Argument(s)`);
   console.log(getTime(), `Loaded ${bot.commands.size} Command(s)`);
-  console.log(getTime(), `Loaded ${Object.keys(bot.eventHandlers).length} Event(s)`);
+  console.log(
+    getTime(),
+    `Loaded ${Object.keys(bot.eventHandlers).length} Event(s)`,
+  );
   console.log(getTime(), `Loaded ${bot.inhibitors.size} Inhibitor(s)`);
   console.log(getTime(), `Loaded ${bot.monitors.size} Monitor(s)`);
   console.log(getTime(), `Loaded ${bot.tasks.size} Task(s)`);
@@ -35,7 +44,10 @@ bot.eventHandlers.ready = async function () {
 
   bot.fullyReady = true;
 
-  console.log(getTime(), `[READY] Bot is online and ready in ${cache.guilds.size} guild(s)!`);
+  console.log(
+    getTime(),
+    `[READY] Bot is online and ready in ${cache.guilds.size} guild(s)!`,
+  );
 
   console.log(getTime(), `Preparing Slash Commands...`);
 
@@ -55,7 +67,7 @@ bot.eventHandlers.ready = async function () {
   if (globalCommands.length) {
     console.log(
       getTime(),
-      `Updating Global Slash Commands... Any changes will take up to 1 hour to update on discord.`
+      `Updating Global Slash Commands... Any changes will take up to 1 hour to update on discord.`,
     );
     await upsertSlashCommands(globalCommands).catch(console.log);
   }
@@ -76,12 +88,16 @@ bot.eventHandlers.ready = async function () {
 
           // ADVANCED VERSION WILL ALLOW TRANSLATION
           const name = translate(guild.id, `commands/${cmd.name}:SLASH_NAME`);
-          const description = translate(guild.id, `commands/${cmd.name}:SLASH_DESCRIPTION`);
+          const description = translate(
+            guild.id,
+            `commands/${cmd.name}:SLASH_DESCRIPTION`,
+          );
 
           return {
             name: name === "SLASH_NAME" ? cmd.name : name,
-            description:
-              description === "SLASH_DESCRIPTION" ? cmd.description || "No description available." : description,
+            description: description === "SLASH_DESCRIPTION"
+              ? cmd.description || "No description available."
+              : description,
             options: cmd.slash?.options?.map((option) => {
               const optionName = translate(guild.id, option.name);
               const optionDescription = translate(guild.id, option.description);
@@ -94,10 +110,13 @@ bot.eventHandlers.ready = async function () {
             }),
           };
         }),
-        guild.id
+        guild.id,
       ).catch(console.log);
-      console.log(getTime(), `Updated Guild ${guild.name} (${guild.id}) Slash Commands...`);
-    })
+      console.log(
+        getTime(),
+        `Updated Guild ${guild.name} (${guild.id}) Slash Commands...`,
+      );
+    }),
   );
 
   console.log(getTime(), `[READY] Slash Commands loaded successfully!`);
