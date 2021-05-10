cff4c2daa3d1c1136a41638245b56347d5fa1f2b
ITOH <to@itoh.at>
2021-05-10 15:36:40 +0200

    Update cache.ts

--- cache.ts
+++ cache.ts
@@ -1,19 +1,6 @@
 import { configs } from "./configs.ts";
-import {
-  botId,
-  cache,
-  Collection,
-  DiscordenoMessage,
-  Manager,
-  snowflakeToBigint,
-  Track,
-  ws,
-} from "./deps.ts";
-import {
-  ButtonCollector,
-  MessageCollector,
-  ReactionCollector,
-} from "./src/types/collectors.ts";
+import { botId, cache, Collection, DiscordenoMessage, Manager, snowflakeToBigint, Track, ws } from "./deps.ts";
+import { ButtonCollector, MessageCollector, ReactionCollector } from "./src/types/collectors.ts";
 import { Argument, Command, PermissionLevels } from "./src/types/commands.ts";
 import { CustomEvents } from "./src/types/events.ts";
 import { Monitor } from "./src/types/monitors.ts";
@@ -38,7 +25,7 @@ export const bot = {
     (
       message: DiscordenoMessage,
       // deno-lint-ignore no-explicit-any
-      command: Command<any>,
+      command: Command<any>
     ) => Promise<boolean> | boolean
   >(),
   monitors: new Collection<string, Monitor>(),
@@ -47,7 +34,7 @@ export const bot = {
     (
       message: DiscordenoMessage,
       // deno-lint-ignore no-explicit-any
-      command: Command<any>,
+      command: Command<any>
     ) => Promise<boolean> | boolean
   >(),
   tasks: new Collection<string, Task>(),
@@ -56,7 +43,6 @@ export const bot = {
   musicQueues: new Collection<bigint, Track[]>(),
   loopingMusics: new Collection<bigint, boolean>(),
   lavadenoManager: new Manager(configs.nodes, {
-    userId: botId.toString(),
     send(id, payload) {
       const shardId = cache.guilds.get(snowflakeToBigint(id))?.shardId;
       if (shardId === undefined) return;
