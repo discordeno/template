fae0b73089c87085c6dcda58e92d85cff831453b
ITOH <to@itoh.at>
2021-05-15 13:35:17 +0200

    fix: calculateShardId for bots with 1 shard

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -577,5 +577,7 @@ export async function fetchMember(guildId: bigint, id: bigint | string) {
 }
 
 export function calculateShardId(guildId: bigint) {
+  if (ws.maxShards === 1) return 0;
+
   return Number((guildId >> 22n) % BigInt(ws.maxShards - 1));
 }
