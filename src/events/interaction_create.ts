0e6c4e2888bd2705d4e347a997e2021d4d4c96c9
ITOH <to@itoh.at>
2021-06-07 17:34:08 +0200

    fix: errors

--- src/events/interaction_create.ts
+++ src/events/interaction_create.ts
@@ -1,18 +1,24 @@
-import { DiscordInteractionTypes } from "../../deps.ts";
+import {
+  ComponentInteraction,
+  DiscordInteractionTypes,
+  SlashCommandInteraction,
+} from "../../deps.ts";
 import { processButtonCollectors } from "../utils/collectors.ts";
 import { bot } from "../../cache.ts";
 
 bot.eventHandlers.interactionCreate = function (data, member) {
   // A SLASH COMMAND WAS USED
   if (data.type === DiscordInteractionTypes.ApplicationCommand) {
-    const command = data.data?.name ? bot.commands.get(data.data.name) : undefined;
+    const command = (data as SlashCommandInteraction).data?.name
+      ? bot.commands.get((data as SlashCommandInteraction).data!.name!)
+      : undefined;
     if (!command) return;
 
-    command.slash?.execute(data, member);
+    command.slash?.execute(data as SlashCommandInteraction, member);
   }
 
   // A BUTTON WAS CLICKED
   if (data.type === DiscordInteractionTypes.MessageComponent) {
-    processButtonCollectors(data, member);
+    processButtonCollectors(data as ComponentInteraction, member);
   }
 };
