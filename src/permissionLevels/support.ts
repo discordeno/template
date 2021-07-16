8bad9f95470ccd2ba67ac28cf0c9941611f64eed
cute savage <41384430+remagnedlogeht@users.noreply.github.com>
2021-07-15 22:03:40 -0400

    Update support.ts

--- src/permissionLevels/support.ts
+++ src/permissionLevels/support.ts
@@ -4,5 +4,5 @@ import { PermissionLevels } from "../types/commands.ts";
 
 // The member using the command must be one of the bots support team
 bot.permissionLevels.set(PermissionLevels.BOT_SUPPORT, (message) =>
-  configs.userIds.botSupporters.includes(message.authorId.toString())
+  configs.userIds.botSupporters.includes(message.authorId)
 );
