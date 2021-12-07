dd3da4ebc2aa7e058dda309f44bc65d0cf2c9aa7
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-12-07 10:38:30 -0700

    Fix Interaction Handler

--- minimal/src/events/interaction-create.ts
+++ minimal/src/events/interaction-create.ts
@@ -1,24 +1,41 @@
-import { bgBlack, bgYellow, black, white, InteractionResponseTypes } from "../../deps.ts";
+import {
+  bgBlack,
+  bgYellow,
+  black,
+  white,
+  InteractionResponseTypes,
+  BotWithCache,
+  DiscordenoGuild
+} from "../../deps.ts";
 import { events } from "./mod.ts";
 import { logger } from "../utils/logger.ts";
 import { getGuildFromId } from "../utils/helpers.ts";
-import { bot as realBot } from "../../mod.ts";
 
 const log = logger({ name: "Event: InteractionCreate" });
 
-events.interactionCreate = async (bot, interaction) => {
+events.interactionCreate = async (rawBot, interaction) => {
+  const bot = rawBot as BotWithCache;
+
   if (interaction.data) {
     if (interaction.id) {
-      const guildName = interaction.guildId
-        ? await getGuildFromId(realBot, interaction.guildId).catch((err) => {
-            log.error(err);
-          })
-        : `DM`;
+      let guildName = "Direct Message";
+      let guild = {} as DiscordenoGuild;
+
+      // Set guild, if there was an error getting the guild, then just say it was a DM. (What else are we going to do?)
+      if (interaction.guildId) {
+        const guildOrVoid = await getGuildFromId(bot, interaction.guildId).catch((err) => {
+          log.error(err);
+        });
+        if (guildOrVoid) {
+          guild = guildOrVoid;
+          guildName = guild.name;
+        }
+      }
 
       log.info(
         `[Command: ${bgYellow(black(interaction.data.name as string))} - ${bgBlack(white(`Trigger`))}] by ${
           interaction.user.username
-        }#${interaction.user.discriminator} in ${guildName}`
+        }#${interaction.user.discriminator} in ${guildName}${guildName !== "Direct Message" ? ` (${guild.id})` : ``}`
       );
 
       await bot.helpers.sendInteractionResponse(interaction.id, interaction.token, {
