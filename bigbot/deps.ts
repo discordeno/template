f1f880597ee8313968576b91ebcd4783867dd964
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2021-12-09 16:44:01 +0000

    feat: base bigbot template

---
+++ bigbot/deps.ts
@@ -0,0 +1,8 @@
+// Terminal Colors!
+export * from "https://deno.land/std@0.117.0/fmt/colors.ts";
+// Get data from .env files
+export { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
+export * from "https://deno.land/x/discordeno@13.0.0-rc14/mod.ts";
+// All the Plugins
+export * from "https://deno.land/x/discordeno_helpers_plugin@0.0.1/mod.ts";
+export * from "https://deno.land/x/discordeno_permissions_plugin@0.0.9/mod.ts";
---
+++ bigbot/src/bot/events/interactions/slash/general/ping.ts
@@ -0,0 +1,22 @@
+import { translate } from "../../../../languages/translate.ts";
+import { createCommand } from "../createCommand.ts";
+
+const command = createCommand({
+  name: "PING_NAME",
+  dev: true,
+  description: "PING_DESCRIPTION",
+  execute: async function (bot, interaction) {
+    return await replyToInteraction(
+      bot,
+      interaction,
+      translate(
+        bot,
+        interaction.guildId!,
+        "PING_RESPONSE_WITH_TIME",
+        Date.now() - bot.helpers.snowflakeToTimestamp(interaction.id),
+      ),
+    );
+  },
+});
+
+export default command;
