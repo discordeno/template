c44f9d278cfa555642b71641fffae85b122b82a5
a boi here <41384430+some-boi@users.noreply.github.com>
2021-12-16 00:17:31 +0100

    Update ready.ts

--- minimal/src/events/ready.ts
+++ minimal/src/events/ready.ts
@@ -1,8 +1,10 @@
 import { events } from "./mod.ts";
+import { BotWithCache } from "../../deps.ts";
 import { logger } from "../utils/logger.ts";
 
 const log = logger({ name: "Event: Ready" });
 
-events.ready = () => {
+events.ready = (bot, {guilds}) => {
+  updateCommands(bot as BotWithCache, guilds);
   log.info("Bot Ready");
 };
